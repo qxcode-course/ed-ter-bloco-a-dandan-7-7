cc858c9a24eb13de354b980c0a2bcba95328e613
dandan-7-7 <danielfernandesmb11@gmail.com>
2026-03-21 18:28:46 -0300

    Implementa leitura de figurinhas e contagem de repetidas

--- baruel/go/draft.go
+++ baruel/go/draft.go
@@ -1,5 +1,23 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+    var qt, qf int
+    fmt.Scan(&qt, &qf)
+    figurinhas := make([]int, qt)
+    rep := 0
+    frp := make([]int, 50)
+    for i := 0; i < qf; i++ {
+        fmt.Scan(&figurinhas[i])
+        if(i > 0) {
+            if figurinhas[i] == figurinhas[i-1] {
+                rep++
+                frp[i] = figurinhas[i]
+            }
+        }
+    }
+    if rep == 0 {
+        fmt.Println("N")
+    } else {
+        
+    }
 }
