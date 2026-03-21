7c7d9e2f9151e7759116845e0adfd479736e2a7c
dandan-7-7 <danielfernandesmb11@gmail.com>
2026-03-21 18:29:04 -0300

    Implementa lógica inicial para o movimento da cobra e adiciona testes

--- baruel/go/draft.go
+++ baruel/go/draft.go
@@ -1,23 +1,10 @@
 package main
 import "fmt"
 func main() {
-    var qt, qf int
-    fmt.Scan(&qt, &qf)
-    figurinhas := make([]int, qt)
-    rep := 0
-    frp := make([]int, 50)
-    for i := 0; i < qf; i++ {
-        fmt.Scan(&figurinhas[i])
-        if(i > 0) {
-            if figurinhas[i] == figurinhas[i-1] {
-                rep++
-                frp[i] = figurinhas[i]
-            }
-        }
-    }
-    if rep == 0 {
-        fmt.Println("N")
-    } else {
-        
-    }
+    var totalAlbum, totalPossui int
+
+    contagem := make(map[int]int)
+
+    for i := 0; i < totalPossui; i++ {
+        var num int
 }
---
+++ gomos/go/draft.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main() {
+    var q int
+    var d string
+    fmt.Scan(&q, &d)
+    for i := 0; i < q; i++ {
+        
+    }
+}
