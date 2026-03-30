5cdacf04cafb25ae3ca3a9e2fd9a353d4e0ff845
dandan-7-7 <danielfernandesmb11@gmail.com>
2026-03-29 22:24:59 -0300

    Implementa lógica para manipulação de listas e adiciona funções de utilidade em Go

--- apagando/go/draft.go
+++ apagando/go/draft.go
@@ -1,5 +1,33 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+	var qini, qdeix int
+	fmt.Scan(&qini)
+	pessoas := make([]int, qini)
+
+	for i := 0; i < qini; i++ {
+		fmt.Scan(&pessoas[i])
+
+	}
+	fmt.Scan(&qdeix)
+	sairam := make([]int, qdeix)
+	for i := 0; i < qdeix; i++ {
+		fmt.Scan(&sairam[i])
+	}
+
+	for i := 0; i < qdeix; i++ {
+		for j := 0; j < qini; j++ {
+			if pessoas[j] == sairam[i] {
+				pessoas[j] = 0
+			}
+		}
+	}
+	for i := 0; i < qini; i++ {
+		if pessoas[i] != 0 {
+			fmt.Print(pessoas[i], " ")
+		}
+	}
+    fmt.Println()
 }
