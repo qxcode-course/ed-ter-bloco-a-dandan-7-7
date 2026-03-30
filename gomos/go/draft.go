adc6e978fef017634e8bd4ad0874ab87af1deb28
dandan-7-7 <danielfernandesmb11@gmail.com>
2026-03-30 20:35:59 -0300

    Implementa lógica para manipulação de pessoas e exibição de estado em jogo

--- gomos/go/draft.go
+++ gomos/go/draft.go
@@ -1,10 +1,66 @@
 package main
 import "fmt"
 func main() {
-    var q int
-    var d string
-    fmt.Scan(&q, &d)
-    for i := 0; i < q; i++ {
-        
+    var n, e int
+    fmt.Scan(&n, &e)
+
+
+    pessoas := make([]int, n)
+
+    for i:= 0; i<n;i++ {
+        pessoas[i] = i+1
     }
+
+    var achou bool
+    var valor int
+    for i := 0; i < n; i++ {
+        achou = false
+        fmt.Print("[ ")
+        for j := 0; j < n; j++ {
+            if pessoas[j] != 0 {
+                fmt.Print(pessoas[j])
+            }
+            if pessoas[j] == e {
+                fmt.Print("> ")
+            } else {
+                fmt.Print(" ")
+            }
+        }
+        fmt.Println("]")
+        for j := 0; j < n; j++ {
+            if pessoas[j] == e {
+                for k := j + 1; k < n; k++ {
+                    if pessoas[k] != 0 {
+                        pessoas[k] = 0
+                        achou = true
+                        valor = k + 1
+                        break 
+                    }
+                }
+                if !achou {
+                    for k := 0; k < j; k++ {
+                        if pessoas[k] != 0 {
+                            pessoas[k] = 0
+                            achou = true
+                            valor = k + 1
+                            break 
+                        }
+                    }
+                }
+                if(e == n - 1) {
+                    for k := 0; k < n; k++ {
+                        if pessoas[k] != 0 {
+                            e = k
+                            break
+                        }
+                    }
+                } else {
+                    e = valor
+                }
+                break
+            }
+        }
+    }
+
+
 }
